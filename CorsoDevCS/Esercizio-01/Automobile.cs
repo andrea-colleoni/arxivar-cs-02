43d31e4ab06a9ae098238b5a886fe88898ade5e5
Andrea Colleoni <andrea@colleoni.info>
2020-09-23 17:36:10 +0200

    esercizio

---
+++ CorsoDevCS/Esercizio-01/Automobile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esercizio_01
+{
+    class Automobile
+    {
+		public string Marca { get; set; }
+		public string Modello { get; set; }
+		public int Cilindrata { get; set; }
+		public float CapacitaSerbatoio { get; set; }
+		public float LivelloCarburante { get; set; }
+		public float ConsumoKmLitro { get; set; }
+
+		private bool autoAccesa;
+
+        public Automobile(string marca, string modello, int cilindrata, float capacita, float consumo)
+        {
+			this.Marca = marca;
+			this.Modello = modello;
+			this.Cilindrata = cilindrata;
+			this.CapacitaSerbatoio = capacita;
+			this.ConsumoKmLitro = consumo;
+			this.autoAccesa = false;
+        }
+
+		public void Rifornimento(float litri)
+		{
+			if (this.LivelloCarburante + litri > this.CapacitaSerbatoio)
+            {
+				Console.WriteLine("Troppo carburante!!");
+				return;
+            }
+			this.autoAccesa = true;
+			this.LivelloCarburante += litri;
+		}
+		public void Viaggio(int kilometri)
+		{
+			var l = kilometri / this.ConsumoKmLitro;
+			if ( this.autoAccesa && l > this.LivelloCarburante)
+            {
+				Console.WriteLine($"{this.Marca} {this.Modello} ha finito il carburante!!");
+				this.autoAccesa = false;
+				return;
+            }
+			if (this.autoAccesa)
+			{
+				this.LivelloCarburante -= l;
+				Console.WriteLine($"{this.Marca} {this.Modello} ha viaggiato per {kilometri} km e consulmato {l} lt. di carburante. {this.LivelloCarburante} lt. rimangono nel serbatoio.");
+			}
+			else
+			{
+				Console.WriteLine($"{this.Marca} {this.Modello} è spenta.");
+			}
+		}
+	}
+}
---
+++ CorsoDevCS/Esercizio-01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Esercizio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var automobili = new List<Automobile>();
+            automobili.Add(new Automobile("Fiat", "Tipo", 1500, 34, 18));
+            automobili.Add(new Automobile("Alfa Romeo", "Giulia", 2000, 45, 13));
+            automobili.Add(new Automobile("Lancia", "Y", 1200, 38, 19));
+
+            // var contains = auto.Contains(null);
+
+            foreach (var auto in automobili)
+            {
+                auto.Rifornimento(new Random().Next(10, (int)auto.CapacitaSerbatoio));
+                // l'operazione di cast ad intero di un decimale tronca il decimale
+            }
+            var kmViaggio = 380;
+            const int stepKm = 20;
+
+            while (kmViaggio > 0)
+            {
+                foreach (var auto in automobili)
+                {
+                    auto.Viaggio(stepKm);
+                }
+                kmViaggio -= stepKm;
+            }
+
+        }
+    }
+}
